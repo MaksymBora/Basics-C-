2f28d7ca254ba80c58d3e044c952ffed3b7c0403
MaksymBora <maxboraod@gmail.com>
2024-01-04 17:13:03 +0100

    update

---
+++ LogicOperations/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LogicOperations;
+
+class Program
+{
+    static void Main(string[] arg)
+    {
+        bool isHightTemperature = true;
+        bool hasNoCooling = true;
+
+        if (isHightTemperature && hasNoCooling)
+        {
+            Console.WriteLine("Warning, the processor is overheating");
+        }
+        
+    }
+}
