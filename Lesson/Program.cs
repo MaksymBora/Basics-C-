2686f79827abcdbb247fb509c1f246824ac0cf3c
Benedikt <praktikant@test.de>
2024-01-04 11:43:38 +0100

    initial commit

---
+++ Lesson/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            
+            int a, b;
+            
+            Console.WriteLine("Enter a figure 1");
+            str = Console.ReadLine();
+
+            a = Convert.ToInt32(str);
+            
+            Console.WriteLine("Enter a figure 2");
+
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+
+            int result = a + b;
+            
+            Console.WriteLine("The sum = " + result);
+
+        }
+    }
+}
