6c2e53ce71832efeee5cce33fac7dfb685162613
MaksymBora <maxboraod@gmail.com>
2024-01-10 13:15:51 +0100

    update task 10 test

--- TestTask10/ConverterTests.cs
+++ TestTask10/ConverterTests.cs
@@ -1,9 +1,49 @@
-namespace TestTask10;
+using Task10;
+using Xunit;
 
-public class ConverterTests
+namespace TestTask10
 {
-    [Fact]
-    public void Test1()
+    public class ConverterTests
     {
+        [Fact]
+        public void ConvertToNumbers_ValidInput_ReturnsConvertedNumbers()
+        {
+            // Arrange
+            string input = "CALLME";
+            string expected = "225563";
+
+            // Act
+            string result = PhoneNumberConverter.ConvertToNumbers(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ConvertToNumbers_InvalidCharacter_ThrowsIllegalPhoneNumberException()
+        {
+            // Arrange
+            string input = "123-456";
+            
+            // Act & Assert
+            Assert.Throws<Task10.IllegalPhoneNumberException>(() =>
+            {
+                PhoneNumberConverter.ConvertToNumbers(input);
+            });
+        }
+
+        [Fact]
+        public void ConvertToNumbers_EmptyInput_ReturnsEmptyString()
+        {
+            // Arrange
+            string input = "";
+            string expected = "";
+
+            // Act
+            string result = PhoneNumberConverter.ConvertToNumbers(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
