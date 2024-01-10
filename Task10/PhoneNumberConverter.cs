9767cbe9e72a3a532001e56a643197cb83b4c9e9
MaksymBora <maxboraod@gmail.com>
2024-01-10 12:08:39 +0100

    add task 10

---
+++ Task10/PhoneNumberConverter.cs
@@ -0,0 +1,46 @@
+﻿namespace Task1;
+
+public class IllegalPhoneNumberException : Exception
+{
+    public IllegalPhoneNumberException(string message) : base(message)
+    {
+    }
+}
+
+public class PhoneNumberConverter
+{
+    private static readonly char[] validChars =
+    {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+        'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    };
+
+    private static readonly char[] mappedDigits =
+    {
+        '2', '2', '2', '3', '3', '3', '4', '4', '4',
+        '5', '5', '5', '6', '6', '6', '7', '7', '7',
+        '7', '8', '8', '8', '8', '9', '9', '9', '9'
+    };
+
+    public static string ConvertToNumbers(string phoneNumber)
+    {
+        string result = "";
+
+        foreach (char c in phoneNumber)
+        {
+            char upperC = char.ToUpper(c);
+            if (Array.IndexOf(validChars, upperC) != -1)
+            {
+                int index = Array.IndexOf(validChars, upperC);
+                result += mappedDigits[index];
+            }
+            else
+            {
+                throw new IllegalPhoneNumberException("Invalid character in the phone number sequence");
+            }
+        }
+
+        return result;
+    }
+}
---
+++ Task10/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            string[] phoneNumbers =
+            {
+                "FAKULTAETFIW",
+                "PIZZARUS",
+                "GL00M",
+                "FR?ST",
+                "STADTVERWALTUNG"
+            };
+
+            foreach (string phoneNumber in phoneNumbers)
+            {
+                try
+                {
+                    string convertedNumber = PhoneNumberConverter.ConvertToNumbers(phoneNumber);
+                    Console.WriteLine($"The converted number for '{phoneNumber}' is '{convertedNumber}'");
+                }
+                catch (IllegalPhoneNumberException e)
+                {
+                    Console.WriteLine(e.Message);
+                    // Console.WriteLine($"Invalid character in phone number: '{phoneNumber}'");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
