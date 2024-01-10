45c36c956e2db576b52e50eb255127d648422f8f
MaksymBora <maxboraod@gmail.com>
2024-01-10 11:20:34 +0100

    add task 11

---
+++ Task11/ChangeCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography;
+using System.Threading.Channels;
+
+namespace Task11
+{
+    public interface IChangeCalculator
+    {
+        Dictionary<Coin, int> GetChange(int euros, int cent);
+    }
+    
+    public class ChangeCalculator : IChangeCalculator
+    {
+        private const int CountOfCentInEuro = 100;
+        
+        public static IChangeCalculator GetInstance() => new ChangeCalculator();
+
+        private ChangeCalculator() { }
+        
+        public Dictionary<Coin, int> GetChange(int euros, int cent)
+        {
+            ValidateMoney(euros, cent);
+
+            var coins = new Dictionary<Coin, int>();
+
+            var totalAmount = euros * CountOfCentInEuro + cent;
+
+            var enumValues = Enum.GetValues(typeof(Coin));
+            Array.Reverse(enumValues);
+            
+            foreach (Coin coin in enumValues)
+            {
+                
+                if (totalAmount < (int)coin) continue;
+                coins[coin] = totalAmount / (int)coin;
+                totalAmount  %= (int)coin;
+            }
+
+            return coins;
+        }
+    
+        private void ValidateMoney(int euros, int cent)
+        {
+            if (euros < 0)
+                throw new ArgumentException($"Invalid parameter {nameof(euros)}, it should be more or equal 0.");
+
+            if (cent < 0)
+                throw new ArgumentException($"Invalid parameter {nameof(cent)}, it should be more or equal 0.");
+        }
+    }
+    
+    
+}
---
+++ Task11/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = ChangeCalculator.GetInstance();
+            var coins = calculator.GetChange(3, 50);
+
+            foreach (var (coin, count) in coins)
+                Console.WriteLine($"{coin}: {count}");
+        }
+    }
+}
